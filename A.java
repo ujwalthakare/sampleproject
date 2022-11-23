40beb39357091a9d1cf25baf6a91a198cc4ddd05
ujwalthakare <ujwalthakare2@gmail.com>
2022-11-23 23:47:52 +0530

    first commit

---
+++ A.java
@@ -0,0 +1,26 @@
+package java_1;
+
+import java.util.Arrays;
+
+public class A {
+public static void main(String[] args) {
+	
+	int a[] = {1, 2, 2, 3, 4, 5, 6, 6, 7 ,7, 1 , 2,50,1,7,2,50};
+	Arrays.sort(a);
+//	int temp[] = new int[a.length];
+	
+	int j = 0;
+	
+	for (int i = 0; i < a.length - 1 ; i++) {
+		if (a[i] != a[i+1]) {
+			a[j++] = a[i];
+		}
+	}
+		a[j++] = a[a.length - 1];
+		
+		for (int k = 0; k < j; k++) {
+			System.out.print(" "+a[k]);
+		}
+	}
+}
+
